415d842eeda975e80270bf77d81ed8d1809dc8b7
Kovak <kovac1066@gmail.com>
2013-12-06 12:20:22 -0700

    app based solution for automatically translating strings

--- main.py
+++ main.py
@@ -5,10 +5,6 @@ from flufl.i18n import initialize
 import os
 from flufl.i18n import registry, PackageStrategy
 import translations.messages
-strategy = PackageStrategy('test', translations.messages) 
-application = registry.register(strategy)
-_ = application._
-_.push('fr')
 from kivy.lang import Builder
 from kivy.properties import ObjectProperty
 from kivy.uix.boxlayout import BoxLayout
@@ -18,28 +14,25 @@ class RootWidget(FloatLayout):
     app = ObjectProperty(None)
     layout = ObjectProperty(None, allownone=True)
 
-    def set_english(self):
-    	_.pop()
-    	self.reload_layout()
-
-    def reload_layout(self):
-    	self.remove_widget(self.layout)
-    	self.layout = None
-    	self.layout = Buttons()
-    	self.add_widget(self.layout)
-
-    def set_french(self):
-    	_.push('fr')
-    	self.reload_layout()
-
 
 class Buttons(BoxLayout):
-	pass
+    pass
+
 
 class Testi18nApp(App):
+    _ = ObjectProperty(None, allownone=True)
 
     def build(self):
-        pass
+        strategy = PackageStrategy('test', translations.messages) 
+        application = registry.register(strategy)
+        self._ = application._
+        self.set_language('fr')
+
+    def set_language(self, language_code):
+        self._.push(language_code)
+        _ = self._
+        self._ = None
+        self._ = _
 
 
 if __name__ == '__main__':
